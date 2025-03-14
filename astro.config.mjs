ca388ac22f5c9b53ddffc8fa1dd7d968f4b70bed
aigle-levant <aiglelevant@gmail.com>
2025-03-14 13:06:14 +0530

    "Modified astro.config.mjs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     output: 'static',
     trailingSlash: 'always',
-    site: 'https://devidev.io',
+    site: 'https://aigle-levant.github.io',
+    base: 'dev-portfolio',
 
     // Single page, no prefetch needed
     prefetch: false,
